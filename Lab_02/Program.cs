5f8f1a62ee2954246d23dd6bfb479d849c3e0322
Edward Vozniak <voznyak.ed@gmail.com>
2024-09-22 21:02:25 +0300

    first branch_02 commit

--- Lab_02/Program.cs
+++ Lab_02/Program.cs
@@ -16,13 +16,13 @@ class Lab_2
 
         alpha = alphaDegrees * (Math.PI / 180.0); // converting from degrees to rad
 
-        z1 = 1 - (1.0/4.0) * Math.Pow(Math.Sin(2 * alpha),2) + Math.Cos(2 * alpha);
+        //z1 = 1 - (1.0/4.0) * Math.Pow(Math.Sin(2 * alpha),2) + Math.Cos(2 * alpha);
         z2 = Math.Pow(Math.Cos(alpha),2) + Math.Pow(Math.Cos(alpha),4);
 
-        bool resultsMatch = Math.Abs(z1 - z2) < 1e-8; // using 1e-8 for greater accuracy
+        //bool resultsMatch = Math.Abs(z1 - z2) < 1e-8; // using 1e-8 for greater accuracy
 
-        Console.WriteLine($"z1 = {z1:F8}");
+        //Console.WriteLine($"z1 = {z1:F8}");
         Console.WriteLine($"z2 = {z2:F8}");
-        Console.WriteLine($"Match: {resultsMatch}"); // if there is match for 2 equations then true
+        //Console.WriteLine($"Match: {resultsMatch}"); // if there is match for 2 equations then true
     }
 }
